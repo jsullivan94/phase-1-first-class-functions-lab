124e446625e777f00944d971c3899d0021627bce
James Sullivan <jsullivan8794@gmail.com>
2023-05-02 14:45:31 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+// const returnFirstTwoDrivers = function(array1) {
+// return array1.slice(0, 2)
+// }
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+const returnFirstTwoDrivers = (drivers) => drivers.slice(0, 2);
+
+const returnLastTwoDrivers = (drivers) => drivers.slice(-2);
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(int) {
+    return function x(fare) {
+        return fare * int
+    };
+}
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, newFunc) {
+    return newFunc(drivers);
+};
+
